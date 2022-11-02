24e53eadb11233148dcfe0694191c99254865a80
chma <christian.em@live.dk>
2022-11-02 19:20:10 +0100

    EAT-7 fixed fetch for create restaurant

---
+++ controllers/register.ts
@@ -0,0 +1,126 @@
+import {Knex} from "knex";
+import {Request, Response} from 'express';
+import {RequestInfo, RequestInit} from 'node-fetch';
+
+export interface Registration {
+    email: string,
+    name: string,
+    password: string,
+    description: string,
+    address: Address
+}
+
+export interface Address {
+    streetName: string,
+    streetNumber: number,
+    zipCode: number,
+    city: string,
+    floor?: number
+}
+
+export interface RegistrationDataObject {
+    id: number,
+    email: string,
+    name: string,
+    description: string,
+    address: Address
+}
+
+export interface RestaurantApiObjectResponse {
+    email: string;
+    name: string;
+    description: string;
+    address: AddressApiObjectResponse;
+    image?: string;
+    restaurantId?: number;
+    menu?: RestaurantMenuApiObjectResponse;
+}
+
+export interface AddressApiObjectResponse {
+    restaurantId: number,
+    streetName: string,
+    houseNumber: number,
+    zipCode: number,
+    city: string
+    floor?: number
+}
+
+export interface RestaurantMenuApiObjectResponse {
+    id: number;
+    categories?: RestaurantMenuCategoryApiObjectResponse[];
+}
+
+export interface RestaurantMenuCategoryApiObjectResponse {
+    id: number;
+    title: string;
+    items: RestaurantMenuCategoryItemApiObjectResponse[]
+}
+
+export interface RestaurantMenuCategoryItemApiObjectResponse {
+    name: string,
+    description: string,
+    price: number,
+    image: string
+}
+
+export const fetch = (url: RequestInfo, init?: RequestInit) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(url, init));
+
+export const handleRegister = (req: Request, res: Response, db: Knex, bcrypt: any) => {
+    console.log(req.body);
+    const registration: Registration = req.body;
+    if (!registration.email || !registration.name || !registration.password) {
+        return res.status(400).json('empty fields');
+    }
+    if (registration.password.length > 20) {
+        res.status(400).json('password is too long. Maximum is 49 characters');
+    }
+
+    db.transaction(trx => {
+        trx.select('email').from('login').where('email', registration.email )
+            .then(email => {
+                console.log(email);
+                if (email.length) {
+                    res.status(400).json('Email already exists')
+                    return;
+                }
+                const hash = bcrypt.hashSync(registration.password, 10);
+
+                return trx.insert({hash: hash, email: registration.email})
+                    .into('login')
+                    .then(affectedRows => {
+                        return trx.select('id').from('login').where('email', registration.email).then(returnedId => {
+                            const id: number = returnedId[0];
+                            const finalRegistration: RegistrationDataObject = {
+                                id,
+                                name: registration.name,
+                                email: registration.email,
+                                description: registration.description,
+                                address: registration.address
+                            }
+                            fetch('http://localhost:5000/createRestaurant', ({
+                                method: 'post',
+                                headers: {'Content-Type': 'application/json'},
+                                body: JSON.stringify(finalRegistration)
+                            })).then((response) => {
+                                response.json()
+                                    .then(data => {
+                                    console.log('data: ', data);
+                                })
+                                    .catch(error => {
+                                        console.log(error);
+                                    })
+                                return res.status(200).json(response);
+                            }).catch(error => {
+                                console.log(error);
+                                return res.status(400).json('Unable to register');
+                            })
+                        })
+                    })
+            }).then(trx.commit)
+            .catch(error => {
+                console.log(error);
+                trx.rollback;
+            })
+    })
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,33 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import knex from 'knex';
+import * as bcrypt from 'bcrypt';
+import {handleRegister} from "./controllers/register";
+import express, {Express, Request, Response} from 'express';
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(bodyParser());
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({message: 'Express + TypeScript Server'});
+const db = knex({
+    client: 'mysql2',
+    connection: {
+        host: 'localhost',
+        port:3309,
+        user: 'root',
+        database: 'db'
+    }
 });
+
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.json({message: 'Express + TypeScript Server'});
+})
+
+app.post("/register",  (req: Request, res: Response) => {handleRegister(req, res, db, bcrypt)});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
