8f86ecd18c8254a75c3ef56ca46dc8e701ecedf2
Sathveekan <sathveekan23@gmail.com>
2022-11-09 22:23:12 +0100

    EAT-8 - Implemented response for restaurant login

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import {handleRegister} from "./controllers/register";
 import express, {Express, Request, Response} from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
+import {handleLogin} from "./controllers/login";
 
 dotenv.config();
 
@@ -29,6 +30,8 @@ app.post('/', (req: Request, res: Response) => {
 })
 
 app.post("/register",  (req: Request, res: Response) => {handleRegister(req, res, db, bcrypt)});
+
+app.post("/login",  (req: Request, res: Response) => {handleLogin(req, res, db, bcrypt)});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
